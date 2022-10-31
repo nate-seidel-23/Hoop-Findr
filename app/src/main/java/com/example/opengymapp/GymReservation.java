c2874b2636f6e859cadb03e331d3564c59d42f5e
NSeidel3468 <90778376+NSeidel3468@users.noreply.github.com>
2022-10-31 11:02:14 -0500

    Created gym reservation and gym objects

--- app/src/main/java/com/example/opengymapp/GymReservation.java
+++ app/src/main/java/com/example/opengymapp/GymReservation.java
@@ -1,4 +1,46 @@
 package com.example.opengymapp;
 
 public class GymReservation {
+    private String gymName;
+    private int numCourts;
+    private String address;
+
+    public GymReservation(String gymName, int numCourts, String address) {
+        this.gymName = gymName;
+        this.numCourts = numCourts;
+        this.address = address;
+    }
+
+    GymReservation ymca = new GymReservation("YMCA", 3,
+            "1400 W Northwest Hwy, Palatine, IL 60067");
+
+    GymReservation community = new GymReservation("Palatine Community Center", 2,
+            "1250 E Wood St, Palatine, IL 60067");
+
+    GymReservation birchwood = new GymReservation("Birchwood recreation center", 1,
+            "435 W. Illinois Avenue");
+
+    public String getGymName() {
+        return gymName;
+    }
+
+    public void setGymName(String gymName) {
+        this.gymName = gymName;
+    }
+
+    public int getNumCourts() {
+        return numCourts;
+    }
+
+    public void setNumCourts(int numCourts) {
+        this.numCourts = numCourts;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
