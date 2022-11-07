b9d47101de6b91540ad1119ee676927f33252520
aidanhal22 <90794198+aidanhal22@users.noreply.github.com>
2022-11-07 11:02:52 -0600

    Added intent for creating a reservation

--- app/src/main/java/com/example/opengymapp/SelectGymActivity.java
+++ app/src/main/java/com/example/opengymapp/SelectGymActivity.java
@@ -2,7 +2,9 @@ package com.example.opengymapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SelectGymActivity extends AppCompatActivity {
 
@@ -11,5 +13,12 @@ public class SelectGymActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_gym);
     }
+
+    public void createReservation(View view) {
+        Intent intent = new Intent(SelectGymActivity.this, CreateReservationActivity.class);
+        startActivity(intent);
+    }
+
+
 
 }
