f23efb2bd415cb37a26879b742fdfc2a17830b5c
aidanhal22 <90794198+aidanhal22@users.noreply.github.com>
2022-12-01 15:49:10 -0600

    birchwood google map API has been imported

--- app/src/main/java/com/example/opengymapp/Maps.java
+++ app/src/main/java/com/example/opengymapp/Maps.java
@@ -2,24 +2,37 @@ package com.example.opengymapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class Maps extends AppCompatActivity implements OnMapReadyCallback{
-
+//    private double long;
+//    private double lat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps);
         mapFragment.getMapAsync(this);
+
     }
 
     @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
-
+    public void onMapReady(GoogleMap googleMap) {
+//    if extra.getIntent().message == birchwood {
+            LatLng birchwood = new LatLng(42.0930720186, -88.0558555322);
+            googleMap.addMarker(new MarkerOptions()
+                    .position(birchwood)
+                    .title("Marker at Birchwood Gym"));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(birchwood));
+//        }
     }
 }
--- app/src/main/java/com/example/opengymapp/SelectGymActivity.java
+++ app/src/main/java/com/example/opengymapp/SelectGymActivity.java
@@ -86,4 +86,5 @@ public class SelectGymActivity extends AppCompatActivity {
     }
 
 
+
 }
