df8b1ce7fc202386c9656733e40c02dddd1c8516
NSeidel3468 <90778376+NSeidel3468@users.noreply.github.com>
2022-12-01 21:35:50 -0600

    Added a spinner, made it so user can only select date ahead of time, set the visibility of the spinner to show only when valid date is selected. Started working on displaying users name on page when they confirm reservation, but need to finish.

--- app/src/main/java/com/example/opengymapp/CreateReservationActivity.java
+++ app/src/main/java/com/example/opengymapp/CreateReservationActivity.java
@@ -3,38 +3,38 @@ package com.example.opengymapp;
 import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.lang.reflect.Array;
 import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
-
-import android.app.DatePickerDialog;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import java.text.DateFormat;
-import java.util.Calendar;
 
-public class CreateReservationActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+public class CreateReservationActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, AdapterView.OnItemSelectedListener {
 
-    private String currentDateString = "";
+    private String selectedDateString = "";
     private ArrayList<String> teamOneNames;
     private ArrayList<String> teamTwoNames;
     private EditText playerNameText;
     private String name;
     GymReservation reservation;
+    TextView t;
+    Spinner spinner;
+    String spinnerSelectedText = "none";
+    public final String TAG = "Denna";
 
 
     @SuppressLint("RestrictedApi")
@@ -55,6 +55,16 @@ public class CreateReservationActivity extends AppCompatActivity implements Date
             }
         });
 
+        spinner = findViewById(R.id.timeSelector);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.spinner_list,
+                getResources().getStringArray(R.array.weekdayTimes));
+
+        adapter.setDropDownViewResource(R.layout.custom_spinner_dropdown_row);
+
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+        spinner.setVisibility(View.INVISIBLE);
+
         teamOneNames = new ArrayList<String>(Arrays.asList("", "" ,"", "", ""));
         teamTwoNames = new ArrayList<String>(Arrays.asList("", "" ,"", "", ""));
         reservation = new GymReservation("","","","","");
@@ -62,22 +72,55 @@ public class CreateReservationActivity extends AppCompatActivity implements Date
     }
 
 
+
+
     @Override
     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
         Calendar c = Calendar.getInstance();
+
         c.set(Calendar.YEAR, year);
         c.set(Calendar.MONTH, month);
         c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-        currentDateString = DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
-        reservation.setDate(currentDateString);
+        selectedDateString = DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
+        Log.d(TAG, " " + c.get(Calendar.YEAR) + c.get(Calendar.DAY_OF_MONTH));
+
+        Calendar today = Calendar.getInstance();
+        Date currentDate = new Date(today.get(Calendar.MONTH), today.get(Calendar.DAY_OF_MONTH),
+                today.get(Calendar.YEAR));
+        Log.d(TAG,  "" + currentDate.getDay());
+
+        if(currentDate.getDay() < dayOfMonth &&
+                currentDate.getMonth() <= month && currentDate.getYear() == year){
+            reservation.setDate(selectedDateString);
+            TextView t = findViewById(R.id.dateText);
+            t.setText(selectedDateString);
+            spinner.setVisibility(View.VISIBLE);
+        }else{
+            Toast.makeText(getApplicationContext(),"Must be a future date",
+                    Toast.LENGTH_LONG).show();
+        }
+
 
     }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        spinnerSelectedText = parent.getItemAtPosition(position).toString();
+        if(timeSelected()) {
+            TextView t = findViewById(R.id.timeText);
+            t.setText(spinnerSelectedText);
+        }
+    }
+
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) { }
 
     public void confirmReservationClickedT1(View view) {
         playerNameText = findViewById(R.id.playerName);
         name = playerNameText.getText().toString();
-
-        reservation.setDate(currentDateString);
+        reservation.setTime(spinnerSelectedText);
+        reservation.setDate(selectedDateString);
 
         if(reservation.getPlayerName().equals("")) {
             Boolean b = false;
@@ -87,7 +130,10 @@ public class CreateReservationActivity extends AppCompatActivity implements Date
                     teamOneNames.set(i, name);
                     reservation.setPlayerName(name);
                     b = true;
+                    t = (TextView)findViewById(R.id.playerName + i + 1);
+                    t.setText(name);
                 }
+
             }
 
             if(b == false){
@@ -98,6 +144,8 @@ public class CreateReservationActivity extends AppCompatActivity implements Date
             Toast.makeText(getApplicationContext(), "You already reserved a spot " +
                     "in this game.", Toast.LENGTH_LONG).show();
         }
+
+
 
         WelcomeActivity.firebaseHelper.addData(reservation);
     }
@@ -136,6 +184,20 @@ public class CreateReservationActivity extends AppCompatActivity implements Date
             }else{
                 return false;
             }
+    }
+
+    public boolean timeSelected(){
+
+        String[] weekend = getResources().getStringArray(R.array.weekdayTimes);
+        boolean optionChose = false;
+
+        for(int i = 1; i < weekend.length - 1; i++)
+        if(spinnerSelectedText.equals(getResources().
+                getStringArray(R.array.weekdayTimes)[i])){
+            optionChose = true;
+        }
+
+        return optionChose;
     }
 
 }
--- app/src/main/java/com/example/opengymapp/Date.java
+++ app/src/main/java/com/example/opengymapp/Date.java
@@ -1,37 +1,37 @@
 package com.example.opengymapp;
 
 public class Date {
-    private String month;
-    private String day;
-    private String year;
+    private int month;
+    private int day;
+    private int year;
 
-    public Date(String month, String day, String year) {
+    public Date(int month, int day, int year) {
         this.month = month;
         this.day = day;
         this.year = year;
     }
 
-    public String getMonth() {
+    public int getMonth() {
         return month;
     }
 
-    public void setMonth(String month) {
+    public void setMonth(int month) {
         this.month = month;
     }
 
-    public String getDay() {
+    public int getDay() {
         return day;
     }
 
-    public void setDay(String day) {
+    public void setDay(int day) {
         this.day = day;
     }
 
-    public String getYear() {
+    public int getYear() {
         return year;
     }
 
-    public void setYear(String year) {
+    public void setYear(int year) {
         this.year = year;
     }
 }
