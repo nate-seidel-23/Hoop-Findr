5b3b898d5ee0bc3d19323505cda559b337d0ddb7
aidanhal22 <90794198+aidanhal22@users.noreply.github.com>
2022-12-06 11:45:29 -0600

    finished most options

--- app/src/main/java/com/example/opengymapp/Maps.java
+++ app/src/main/java/com/example/opengymapp/Maps.java
@@ -2,9 +2,15 @@ package com.example.opengymapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuBuilder;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -52,6 +58,28 @@ public class Maps extends AppCompatActivity implements OnMapReadyCallback{
                 .title("Marker at " + name + " Gym"));
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(birchwood));
 
+    }
+    @SuppressLint("RestrictedApi")
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_hoopfindr, menu);
+
+        if(menu instanceof MenuBuilder){
+            MenuBuilder m = (MenuBuilder) menu;
+            m.setOptionalIconsVisible(true);
+        }
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.back:
+                Intent intent1 = new Intent(this, SelectGymActivity.class);
+                this.startActivity(intent1);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
 
